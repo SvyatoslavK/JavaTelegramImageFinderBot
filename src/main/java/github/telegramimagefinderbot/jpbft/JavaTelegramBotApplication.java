50f4495438103be3f47704ae2d7cac1666fbb055
Svyatoslav <sk10000@yandex.ru>
2022-01-13 00:08:23 +0300

    Add maven and spring boot files svyat.kob@ya.ru 13.01.2022

---
+++ src/main/java/github/telegramimagefinderbot/jpbft/JavaTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package github.telegramimagefinderbot.jpbft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTelegramBotApplication.class, args);
+	}
+
+}
