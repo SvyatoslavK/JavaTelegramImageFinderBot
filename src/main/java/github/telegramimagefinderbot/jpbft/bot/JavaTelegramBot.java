8632ed26c42ba065ec56ce99a99495130fd404d7
Svyatoslav <32942079+SvyatoslavK@users.noreply.github.com>
2022-01-15 00:12:14 +0300

    Add photo "echo" to telegram bot

--- src/main/java/github/telegramimagefinderbot/jpbft/bot/JavaTelegramBot.java
+++ src/main/java/github/telegramimagefinderbot/jpbft/bot/JavaTelegramBot.java
@@ -7,10 +7,13 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Telegram bot for uploading pictures from sites to telegram channel
@@ -24,29 +27,44 @@ public class JavaTelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if(update.hasMessage() && (update.getMessage().hasText()|update.getMessage().hasPhoto()))
-        {
+
+        String chat_id = update.getMessage().getChatId().toString();
+
+        if (update.hasMessage() && (update.getMessage().hasText())) {
+
             String message = update.getMessage().getText().trim();
-            String chat_id = update.getMessage().getChatId().toString();
 
             SendMessage mess = new SendMessage();
 
             mess.setChatId(chat_id);
             mess.setText(message);
 
-       /*     File obj = update.getMessage().getDocument();
-            SendPhoto messP = SendPhoto.builder().photo(obj) ;*/
-
-
             try {
                 execute(mess);
-            } catch (TelegramApiException e)
-            {
-              e.printStackTrace();
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
+        } else if (update.hasMessage() && update.getMessage().hasPhoto()) {
 
+            List<PhotoSize> photos = update.getMessage().getPhoto();
+
+            String photo_id = photos.get(0).getFileId();
+            InputFile ph = new InputFile().setMedia(photo_id);
+            SendPhoto sent_photo = new SendPhoto();
+
+            sent_photo.setPhoto(ph);
+            sent_photo.setChatId(chat_id);
+
+            try {
+                execute(sent_photo);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         }
+
+
     }
+
 
     @Value("${bot.username}")
     private String username;
